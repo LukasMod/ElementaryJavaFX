3a59941fecf4afac867d5892c048c58fa0e1bbe1
LukasMod <lukaszmod@o2.pl>
2020-01-09 23:48:50 +0100

    JavaFX - Using only Java Code

--- src/Main5/Main.java
+++ src/Main5/Main.java
@@ -1,18 +1,39 @@
 package Main5;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
+        BorderPane borderPane = new BorderPane();
 
-        Parent root = FXMLLoader.load(getClass().getResource("main_window.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root));
+        StackPane stackPaneTop = MainWindowController.stackPaneTop(); //tworzymy obiekt stackpane i wskazujemy lokalizacje
+        borderPane.setTop(stackPaneTop);  // StackPane do BorderPane
+
+        StackPane stackPaneLeft = MainWindowController.stackPaneLeft(); //tworzymy obiekt stackpane i wskazujemy lokalizacje
+        BorderPane.setMargin(stackPaneLeft, new Insets(2, 2, 2, 2)); //marinesy całego stackPane
+        borderPane.setLeft(stackPaneLeft);  // StackPane do BorderPane
+
+        StackPane stackPaneBottom = MainWindowController.stackPaneBottom(); //tworzymy obiekt stackpane i wskazujemy lokalizacje
+        BorderPane.setMargin(stackPaneBottom, new Insets(2, 2, 2, 2)); //marinesy całego stackPane
+        borderPane.setBottom(stackPaneBottom);  // StackPane do BorderPane
+
+        StackPane stackPaneCenter = MainWindowController.stackPaneCenter(); //tworzymy obiekt stackpane i wskazujemy lokalizacje
+        BorderPane.setMargin(stackPaneCenter, new Insets(2, 2, 2, 2)); //marinesy całego stackPane
+        borderPane.setCenter(stackPaneCenter);  // StackPane do BorderPane
+
+
+        Scene scene = new Scene(borderPane);
+        primaryStage.setTitle("Window - only Java code");
+        primaryStage.setOnCloseRequest(windowEvent -> MainWindowController.closeProgram());
+        primaryStage.setWidth(800);
+        primaryStage.setHeight(600);
+        primaryStage.setScene(scene);
         primaryStage.show();
 
     }
--- src/Main5/MainWindowController.java
+++ src/Main5/MainWindowController.java
@@ -1,7 +1,14 @@
 package Main5;
 
+import javafx.application.Platform;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 
+import javax.swing.plaf.MenuBarUI;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -10,4 +17,83 @@ public class MainWindowController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
+
+    public static void closeProgram() {
+        Platform.exit();
+        System.exit(0);
+    }
+
+    private static MenuBar prepareMenu () {
+        MenuBar menu = new MenuBar();
+        Menu menuFile = new Menu("File");  //sekcja w menu
+        MenuItem exitMenuItem = new MenuItem("Exit");
+        exitMenuItem.setOnAction(actionEvent -> MainWindowController.closeProgram());  //wywołanie kliknięciem
+
+        //składamy teraz naze klocki w odwrotnej kolejności
+        menuFile.getItems().add(exitMenuItem);
+        menu.getMenus().add(menuFile);
+
+        return menu; //musi zwrócić nasz obiekt
+    }
+
+    public static StackPane stackPaneTop() {
+        StackPane stackPane = new StackPane();
+        stackPane.setAlignment(Pos.TOP_LEFT);  //orientacja
+        stackPane.getChildren().add(MainWindowController.prepareMenu()); //dodaje menu do stackPane
+        Button b1 = new Button("B1 Close");  //przycisk
+        b1.setOnAction(actionEvent -> MainWindowController.closeProgram()); //przypisanie akcji do przycisku
+        StackPane.setMargin(b1, new Insets(25, 0, 0, 2));  //umiejscowienie przycisku b1
+        stackPane.getChildren().add(b1);  //dodanie przycisku
+
+
+        return stackPane;  //musi zwrócić nasz obiekt
+
+    }
+
+    public static StackPane stackPaneLeft () {
+        StackPane stackPane = new StackPane();
+        stackPane.setAlignment(Pos.TOP_LEFT);  //orientacja
+        //obrysowanie przycisku: kolor, styl, zaokrąglone rogi (promień zagięcia w pikselach),
+        stackPane.setBorder(new Border(new BorderStroke(Color.DARKGOLDENROD, BorderStrokeStyle.DASHED, new CornerRadii(5), BorderWidths.DEFAULT)));
+        stackPane.setPadding(new Insets(4, 4, 4, 4)); //wewnętrzne marginesy w stackPane
+
+        Button b1 = new Button("Button StackPane Left");  //przycisk
+        b1.setPrefWidth(150);
+        stackPane.getChildren().add(b1);  //dodanie przycisku
+
+        Button b2 = new Button("Button Small");  //przycisk
+        stackPane.getChildren().add(b2);  //dodanie przycisku
+        StackPane.setMargin(b2, new Insets(25, 0, 0, 2));  //umiejscowienie przycisku b1
+
+        return stackPane;  //musi zwrócić nasz obiekt
+    }
+
+    public static StackPane stackPaneBottom () {
+        StackPane stackPane = new StackPane();
+        stackPane.setAlignment(Pos.TOP_LEFT);  //orientacja
+        //obrysowanie przycisku: kolor, styl, zaokrąglone rogi (promień zagięcia w pikselach),
+        stackPane.setBorder(new Border(new BorderStroke(Color.DARKORANGE, BorderStrokeStyle.DOTTED, new CornerRadii(1), BorderWidths.DEFAULT)));
+        stackPane.setPadding(new Insets(4, 4, 4, 4)); //wewnętrzne marginesy w stackPane
+
+        Button b1 = new Button("Button Bottom");  //przycisk
+        b1.setPrefWidth(150);
+        stackPane.getChildren().add(b1);  //dodanie przycisku
+
+        return stackPane;  //musi zwrócić nasz obiekt
+    }
+
+    public static StackPane stackPaneCenter () {
+        StackPane stackPane = new StackPane();
+        stackPane.setAlignment(Pos.TOP_LEFT);  //orientacja
+        //obrysowanie przycisku: kolor, styl, zaokrąglone rogi (promień zagięcia w pikselach),
+        stackPane.setBorder(new Border(new BorderStroke(Color.DARKORANGE, BorderStrokeStyle.SOLID, new CornerRadii(10), BorderWidths.DEFAULT)));
+        stackPane.setPadding(new Insets(4, 4, 4, 4)); //wewnętrzne marginesy w stackPane
+
+        TextArea textArea = new TextArea();
+        textArea.setWrapText(true); //zawijanie tekstu w polu tekstowym
+        stackPane.getChildren().add(textArea);  //dodanie pola tekstowego
+
+        return stackPane;  //musi zwrócić nasz obiekt
+    }
+
 }
